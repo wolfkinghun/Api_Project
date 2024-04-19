f767b9b934d54b3626482233f9fa5751702c2dc2
Henlorata <matuzabalazs@gmail.com>
2024-04-19 09:42:52 +0200

    Api start

---
+++ getData.js
@@ -0,0 +1,10 @@
+/*export const getData = async (url, options) => {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+*/
