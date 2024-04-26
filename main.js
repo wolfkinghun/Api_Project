922855d9a057de984480b55caaeba20084a9968e
Henlorata <matuzabalazs@gmail.com>
2024-04-26 10:08:15 +0200

    redirect function in progress, needs rewriting

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function renderPage() {
 														<div class="article-body">
 															<h2>${data[pageData].title}</h2>
 															
-															<a href="" class="read-more" onclick="${redirect(data[pageData])}">
+															<a href="" class="read-more" onclick="redirect(event, ${data[pageData]})">
 															Read more <span class="sr-only">about this</span>
 															<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 20 20" fill="currentColor">
 																<path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -109,19 +109,6 @@ function checkContent() {
 }
 
 function redirect(array) {
-	fetch('secondary.html', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(array)
-	}).then(response => {
-		if (!response.ok) {
-			throw new Error('Network response was NOT okay!')
-		}
-		window.location.href = 'secondary.html'
-	})
-	.catch(error => {
-		console.error('There was a problem with the fetch operation.', error)
-	})
-}
+	localStorage.setItem('data', JSON.stringify(array))
+	window.location.href = 'secondary.html'
+  }
--- secondary.js
+++ secondary.js
@@ -1,16 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const arrayPromise = new Promise((resolve, reject) => {
-        const jsonData = window.localStorage.getItem('jsonData')
-        try {
-            const myArray = JSON.parse(jsonData)
-            resolve(myArray)
-        } catch(error) {
-            reject(error)
-        }
-    })
-    arrayPromise.then(myArray => {
-        console.log('Received array: ' + myArray);
-    }).catch(error => {
-        console.error('Error: ', error)
-    }) 
+    const data = JSON.parse(localStorage.getItem('data'));
+    console.log(data);
 })
